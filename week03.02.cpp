461ace857de70ae588dd91770dbb5d29f0625a5d
yu0986 <79677216+yu0986@users.noreply.github.com>
2021-03-12 12:01:20 +0800

    Add files via upload

--- week03.02.cpp
+++ week03.02.cpp
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+int a[5]={0,10,20,30,40};
+void printAll()
+{
+    for(int i=0;i<5;i++) printf("%d ",a[i]);
+    printf("\n");
+}
+int main()
+{
+        printAll();
+    int *p=&a[2];
+    *p=222;
+        printAll();
+    p=p+2;
+    *p=666;
+        printAll();
+    p--;
+    *p=555;
+        printAll();
+}
